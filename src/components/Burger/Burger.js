b02fee98b258790657fa4ba6a7a1a04bd7a6cfa3
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-10 20:14:34 -0600

    8.120 flattened transformedIngredients to single array.  added if statement to handle  scenario when no toppings in state to render prompt for user.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,12 +7,20 @@ const burger = (props) => {
 
 
   // Step-through explaination:  https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/questions/3501504
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map( igKey => { // igKey: "salad"
       return [...Array(props.ingredients[igKey])].map( (_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
     })
+    .reduce((arr, el) => {  // (previous value, current value), reduce method accepts call back of everything before and initial value
+      return arr.concat(el)
+    }, [])
+
+  // console.log(transformedIngredients)
+  if (transformedIngredients == 0 ) {
+    transformedIngredients = <p>Please start adding ingredients.</p>
+  }
 
   return(
     <div className={classes.Burger}>
