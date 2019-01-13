0be5bd633396affed2983b0d53174a996fab53c0
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-13 02:05:41 -0600

    8.129 added model css and animation logic.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,9 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -26,7 +29,8 @@ class BurgerBuilder extends Component {
       meat: 0
     },
     totalPrice: 4,
-    purchaseable: false
+    purchaseable: false,
+    purchasing: false
   }
 
   updatePurchaseState(ingredients) {
@@ -71,6 +75,10 @@ class BurgerBuilder extends Component {
     this.updatePurchaseState(updatedIngredients);
   }
 
+  purchaseHandler = () => {
+    this.setState({purchasing: true})
+  }
+
   render() {
 
     const disabledInfo = {
@@ -82,13 +90,17 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
+        <Modal show={this.state.purchasing}>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
             ingredientAdded={this.addIngredientHandler}
             ingredientRemoved={this.removeIngredient}
             disabled={disabledInfo}
             price={this.state.totalPrice}
-            purchaseable={this.state.purchaseable} />
+            purchaseable={this.state.purchaseable}
+            ordered={this.purchaseHandler} />
       </Aux>
     );
   }
