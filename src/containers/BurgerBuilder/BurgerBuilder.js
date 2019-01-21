3556256f734d85602efc48517633924cead4c366
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-20 18:02:25 -0600

    10.172 successfully added post request via axios for purchasing order.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Axios from '../../axios-orders';
 
 
 const INGREDIENT_PRICES = {
@@ -84,7 +85,23 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    alert('Continue.')
+    // alert('Continue.')
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'First Last',
+        address: {
+          street: '1st Ave',
+          zipCode: '123456',
+          country: 'USA'
+        },
+        deliveryMethod: 'fastest'
+      }
+    }
+    Axios.post('/orders.json', order)
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
   }
 
   render() {
