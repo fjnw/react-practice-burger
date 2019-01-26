a33362183eb6c193dfef20392f90a743958a30ca
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-25 21:40:26 -0600

    12.217 added contact data form actived by route.

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,34 @@
+import React, { Component} from 'react';
+
+import classes from './ContactData.module.css'
+import Button from '../../../components/UI/Button/Button';
+
+class ContactData extends Component {
+  state = {
+    name: '', 
+    email: '',
+    address: {
+      street: '', 
+      postalCode: ''
+    }
+  }
+
+  render () {
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Data</h4>
+        <form>
+          <input className={classes.Input} type="email" name="email" placeholder="Your email" />
+          <input className={classes.Input} type="text" name="street" placeholder="Your street" />
+          <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+          <input className={classes.Input} type="text" name="postal" placeholder="Your zip code" />
+          <Button btnType="Success">Order</Button>
+        </form>
+      </div>
+    )
+  }
+
+}
+
+export default ContactData;
+
