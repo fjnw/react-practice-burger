a9ace5411ab976043b409ddb11e8ebc452320247
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-31 21:15:04 -0600

    13.235 added feedback to field validation excluding dropdown since there's always a value there.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -17,9 +17,9 @@ class ContactData extends Component {
         },
         value: '',
         validation: {
-          required: true,
-          valid: false
-        }
+          required: true
+        },
+        valid: false
       },
       street: {
         elementType: 'input',
@@ -29,9 +29,9 @@ class ContactData extends Component {
         },
         value: '',
         validation: {
-          required: true,
-          valid: false
-        }
+          required: true
+        },
+        valid: false
       },
       zipCode: {
         elementType: 'input',
@@ -42,10 +42,10 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true,
-          valid: false,
           minLength: 5,
           maxLength: 5
         },
+        valid: false
       },
       country: {
         elementType: 'input',
@@ -55,9 +55,9 @@ class ContactData extends Component {
         },
         value: '',
         validation: {
-          required: true,
-          valid: false
-        }
+          required: true
+        },
+        valid: false
       },
       email: {
         elementType: 'input',
@@ -67,9 +67,9 @@ class ContactData extends Component {
         },
         value: '',
         validation: {
-          required: true,
-          valid: false
-        }
+          required: true
+        },
+        valid: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -90,12 +90,15 @@ class ContactData extends Component {
     if (rules.required) {
       isValid = value.trim() !== "" && isValid;
     }
+
     if (rules.minLength) {
       isValid = value.length >= rules.minLength && isValid;
     }
+
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
     }
+    
     return isValid;
   }
 
@@ -134,7 +137,7 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
     }
     updatedFormElement.value = event.target.value;
-    updatedFormElement.validation.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log(updatedFormElement)
     this.setState({orderForm: updatedOrderForm})
@@ -157,6 +160,8 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
             changed={(event) =>this.inputChangedHandler(event, formElement.id)} />
         ))}
       <Button btnType="Success" >Order</Button>
