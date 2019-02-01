822b36ebf3a382c21bbbc9cd360c1e05d91f8d87
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-31 19:38:19 -0600

    13.231 updating state with form data in ContactData.js in an immutable way.

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,6 +83,19 @@ class ContactData extends Component {
     });
   }
 
+  inputChangedHandler = (event, inputIdentifier) => {
+    // console.log(event.target.value, inputIdentifier)
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = { 
+      ...updatedOrderForm[inputIdentifier]
+    }
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm})
+  }
+
   render () {
     const formElementArray = [];
     for (let key in this.state.orderForm) {
@@ -99,7 +112,8 @@ class ContactData extends Component {
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
-            value={formElement.config.value} />
+            value={formElement.config.value}
+            changed={(event) =>this.inputChangedHandler(event, formElement.id)} />
         ))}
       <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
     </form>
