5251768c57398f72d3a4e73a91a43019581e7168
Jamal <faizanjamal2018@u.northwestern.edu>
2019-02-05 18:51:34 -0600

    15.270 got rid of passing ingredients by params and used redux for ingredients and price in checkout.js and contactdata.js

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 
 import classes from './ContactData.module.css'
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -120,7 +121,7 @@ class ContactData extends Component {
     }
 
     const order = {
-      ingredients: this.props.ingredients,
+      ingredients: this.props.ings,
       price: this.props.price,
       orderData: formData
     }
@@ -195,5 +196,12 @@ class ContactData extends Component {
 
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  }
+}
+
+export default connect(mapStateToProps)(ContactData);
 
